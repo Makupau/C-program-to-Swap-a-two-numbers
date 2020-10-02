81c69ccc4545a811da874f81d894b4e372800bfb
Diwash <71179725+Diwash061@users.noreply.github.com>
2020-10-02 22:12:58 +0545

    Update WAP to swap number.cpp

--- WAP to swap number.cpp
+++ WAP to swap number.cpp
@@ -3,11 +3,11 @@
 #include<conio.h>
 main()
 {
-    int x = 10, y = 15, temp;
+    int x = 10, y = 15;
     printf("Before Swap x = 10 and y = 15\n");
-    temp = x;
-    x = y;
-    y = temp;
+    x=x*y;
+    y=x/y;
+    x =x/y;
     printf("After Swap x = %d and y = %d", x, y);
     getch();
 }
